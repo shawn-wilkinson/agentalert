bbab4d3de74915f22a9c2de535bf50770ff1cd2a
Shawn Wilkinson <shawnwilkinson11@gmail.com>
2015-10-21 18:30:58 -0500

    debug interface model

--- app/models/interface.rb
+++ app/models/interface.rb
@@ -4,23 +4,21 @@ class Interface < ActiveRecord::Base
   @@client = Twilio::REST::Client.new ENV['ACCOUNT_SID'], ENV['AUTH_TOKEN']
   @@from ='+15854818275'
 
-  def process_text
+  def self.process_text
     p params
   end
 
-  def test_user_number(name,phone_number)
+  def self.test_user_number(name,phone_number)
     text = "Hello #{name}, this is a test SMS from Agent Alert."
     send_text(phone_number,text)
   end
 
-  def test_contact_number(name,phone_number)
+  def self.test_contact_number(name,phone_number)
     text = "Hello #{name}, this is a test SMS from Agent Alert."
     send_text(phone_number,text)
   end
 
-  # def test
-
-  # def send_alert
+  # def self.send_alert
   #   @user = User.find(params[:id])
   #   if params[:note]
   #     note = params[:note]
@@ -29,7 +27,7 @@ class Interface < ActiveRecord::Base
 
   private
 
-  def send_text(number,text)
+  def self.send_text(number,text)
     message = @@client.account.messages.create({
               :from => @@from,
               :to => number,
