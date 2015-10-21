087190c34bdda0b3f2fb36da224931729d5a33ac
Shawn Wilkinson <shawnwilkinson11@gmail.com>
2015-10-21 18:34:17 -0500

    clean up interface controller

--- app/controllers/interface_controller.rb
+++ app/controllers/interface_controller.rb
@@ -1,9 +1,4 @@
-require 'twilio-ruby'
-
 class InterfaceController < ApplicationController
-
-  @@client = Twilio::REST::Client.new ENV['ACCOUNT_SID'], ENV['AUTH_TOKEN']
-  @@from ='+15854818275'
 
   def process_text
     p params
@@ -12,17 +7,12 @@ class InterfaceController < ApplicationController
   def test_user_number
     @user = User.find(params[:id])
     Interface.test_user_number(@user.name,@user.phone_number)
-    # @user = User.find(params[:id])
-    # text = "Hello #{@user.name}, this is a test SMS from Agent Alert."
-    # send_text(@user.phone_number,text)
     redirect_to "/users/#{params[:id]}"
   end
 
   def test_contact_number
     @contact = Contact.find(params[:id])
     Interface.test_contact_number(@contact.name,@contact.phone_number)
-    # text = "Hello #{@contact.name}, this is a test SMS from Agent Alert."
-    # send_text(@contact.phone_number,text)
     redirect_to "/users/#{@contact.user.id}"
   end
 
@@ -32,17 +22,5 @@ class InterfaceController < ApplicationController
   #     note = params[:note]
   #   end
   # end
-
-  private
-
-  def send_text(number,text)
-    message = @@client.account.messages.create({
-              :from => @@from,
-              :to => number,
-              :body => text
-    })
-
-  end
-
 
 end
