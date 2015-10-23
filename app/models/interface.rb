cc25d2c20a177210cc1f617a42df35624215d4b6
Shawn Wilkinson <shawnwilkinson11@gmail.com>
2015-10-22 19:52:02 -0500

    start functionality for panic word detection and contact notification

--- app/models/interface.rb
+++ app/models/interface.rb
@@ -5,7 +5,17 @@ class Interface < ActiveRecord::Base
   @@from ='+15854818275'
 
   def self.process_text(from,body)
-    return "Thanks for texting agent alert #{body} from #{from}"
+    if user = User.find(cleaned_number(from))
+      if body.include?(user.panic_word)
+        alert_contacts(user)
+        return "Your contacts have been alerted."
+      else
+        return "Thanks for texting Agent Alert, #{@user.name}"
+      end
+    else
+      return "Thanks for texting agent alert #{body} from #{from}"
+    end
+
   end
 
   def self.test_user_number(name,phone_number)
@@ -16,6 +26,12 @@ class Interface < ActiveRecord::Base
   def self.test_contact_number(name,phone_number)
     text = "Hello #{name}, this is a test SMS from Agent Alert."
     send_text(phone_number,text)
+  end
+
+  def self.alert_contacts(user)
+    user.contacts.each do |contact|
+      send_alert(contact.name,user.name,contact.phone_number)
+    end
   end
 
   def self.send_alert(contact_name,user_name,phone_number,note=nil)
@@ -35,6 +51,10 @@ class Interface < ActiveRecord::Base
               :to => number,
               :body => text
     })
+  end
+
+  def cleaned_number(number)
+    number[2..-1]
   end
 
 end
