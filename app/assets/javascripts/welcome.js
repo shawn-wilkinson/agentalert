8eb5d4d55fb91ae47c34258519262654e1a265b0
Shawn Wilkinson <shawnwilkinson11@gmail.com>
2015-11-04 16:39:07 -0600

    add image slider on main page - still working on perfecting js for it

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -1,6 +1,6 @@
 $(document).ready(function(event){
 
-
+  //FAQ ACCORDION JQUERY
 
   $('.answer').hide();
 
@@ -11,4 +11,44 @@ $(document).ready(function(event){
 
   });
 
+
+  // IMAGE SLIDER ON INDEX PAGE
+
+  //Set Options
+
+
+
+  //Add initial active class
+  $('.slide').first().addClass('active');
+
+  //Hide all slides
+  $('.slide').hide();
+
+  //Show first slide
+  $('.active').show();
+
+
+  //Auto Slider Handler
+
+
+  setInterval(nextSlide,7000);
+
+  function nextSlide(){
+    $('.active').removeClass('active').addClass('oldActive')
+
+    if($('.oldActive').is(':last-child')){
+      $('.slide').first().addClass('active');
+      console.log('going back to the beginning...');
+    } else {
+      $('.oldActive').next().addClass('active');
+      console.log('next slide!');
+    };
+    $('.oldActive').removeClass('oldActive');
+    $('.slide').fadeOut(700);
+    $('.active').fadeIn(700);
+  }
+
+
+
 });
+
